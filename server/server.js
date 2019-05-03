9e1740ae62606b6ba49f1a036e569ae47391167d
jmargielewski <kuba.margielewski@gmail.com>
2019-05-03 19:55:35 +0100

    server init

---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema/schema');
+
+const { USER, KEY } = require('./config');
+const app = express();
+
+const MONGO_URI = `mongodb://${USER}:${KEY}@ds149676.mlab.com:49676/auth-graphql`;
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'aaabbbccc',
+    store: new MongoStore({
+      url: MONGO_URI,
+      autoReconnect: true,
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+  '/graphql',
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+module.exports = app;
